404d20a5ce1ff8cd8ccd2f996c052ca4c9cdf963
ShivaniAdimulam <adimulamshivani@gmail.com>
2022-07-06 23:38:11 +0530

    controller added

--- Desktop/BAssignment/src/controller/controller.js
+++ Desktop/BAssignment/src/controller/controller.js
@@ -1,1 +1,23 @@
 const axios=require('axios')
+
+const getrepo=async function(req,res){
+    try{
+    const{since,language}=req.query;
+    let filter={}
+    if(since){ 
+        filter["since"]=since
+    }
+
+    if(language){
+        filter["language"]=language
+    }
+
+    const data=await axios.get("https://private-d1997-githubtrendingapi.apiary-mock.com/repositories",{filter})
+
+    return res.status(200).send({Data:data})
+
+    }catch(err){
+        console.log(err.message)
+    }
+
+}
